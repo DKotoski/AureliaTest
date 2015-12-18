a7a8e8efc872ea080c44927f4f23be4500f244a5
Darko Kotoski <wbountyhunter@hotmail.com>
2015-12-18 15:40:14 +0100

    Initial Commit
    This is a starter showcase for the Aurelia framework.

---
+++ src/app.js
@@ -0,0 +1,12 @@
+﻿export class App {
+    configureRouter(config, router) {
+        config.title = 'Aurelia';
+        config.map([
+          { route: ['','welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' },
+          { route: ['something'], name: 'something', moduleId: './something', nav: true, title:'Something' },
+          { route: 'users', name: 'users', moduleId: './users', nav: true, title:'Github Users' }
+        ]);
+
+        this.router = router;
+    }
+}
---
+++ src/something.js
@@ -0,0 +1,3 @@
+﻿export class Something {
+    aProperty = "property it was";
+}
